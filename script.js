af127a5fdb4a95519b6e9aa334354c4a93b31dfb
Alisson <arisutvyt@gmail.com>
2022-08-07 19:57:14 -0300

    nova base
    meu pc n suporta node ent tirei o phaser

--- script.js
+++
@@ -1,9 +0,0 @@
-(function(){
-    var game = new Phaser.Game(800, 600, Phaser.AUTO, 'container', {preload:preload,create:create,update:update});
-
-    function preload(){}
-
-    function create(){}
-
-    function update(){}
-}());

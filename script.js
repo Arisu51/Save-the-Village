cb441528b42a695c63861e601831cb98a92f4650
Alisson <arisutvyt@gmail.com>
2022-08-11 20:19:33 -0300

    animações e sprites adicionados

--- script.js
+++ script.js
@@ -28,43 +28,63 @@ function control() {
     player.style.left = xPlayerPos + 'px';
 }
 
+function jump() {
+
+}
+
 function mover() {
     tecla = event.keyCode;
     switch (tecla) {
         case 37:
-            dx = -1;
             if (powerUp) {
                 player.src = 'img/pixil_2.png';
+                dx = -2;
+                xPlayerPos += dx*5;
             } else {
                 player.src = 'img/pixil_1.png';
+                dx = -1;
+                xPlayerPos += dx*5;
             }
             break;
         case 39:
-            dx = 1;
             if (powerUp) {
                 player.src = 'img/pixil_8.png';
+                dx = 2;
+                xPlayerPos += dx*5;
             } else {
                 player.src = 'img/pixil_7.png';
+                dx = 1;
+                xPlayerPos += dx*5;
+            }
+            break;
+        case 40:
+            break;
+        case 38:
+            if (powerUp) {
+                player.src = 'img/pixil_6.png';
+            } else {
+                player.src = 'img/pixil_5.png';
+                player.classList.add('jump');
             }
             break;
         default:
             break;
     }
-    xPlayerPos += dx*5;
 }
 function parar() {
     tecla = event.keyCode;
     switch (tecla) {
         case 37:
-            dx = -1;
+            dx = 0;
+            xPlayerPos += dx*5;
             break;
         case 39:
-            dx = 1;
+            dx = 0;
+            xPlayerPos += dx*5;
             break;
         default:
             break;
     }
-    xPlayerPos += dx*5;
     if (powerUp) {
         player.src = 'img/pixil_4.png';
     } else {
